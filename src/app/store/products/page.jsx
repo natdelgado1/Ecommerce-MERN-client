77b953b18e6f00fc804168f1def74991fd3d197c
Natalia Delgado <ilanaadelgad0@gmail.com>
2024-03-06 21:55:05 -0300

    Refactorización de la página de productos

--- src/app/store/products/page.jsx
+++ src/app/store/products/page.jsx
@@ -1,16 +1,11 @@
 import AllProducts from "@/components/AllProducts/AllProducts";
 
-export default function ProductsPage(){
-
-
-    return (
-        <div className="px-28 py-7">
-
-        
-        <div>
-            <AllProducts title= "Productos"></AllProducts>
-            
-        </div>
-        </div>
-    );
+export default function ProductsPage() {
+  return (
+    <div className="px-28 py-7">
+      <div>
+        <AllProducts title="Productos"></AllProducts>
+      </div>
+    </div>
+  );
 }
